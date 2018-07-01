db4b988a246190c537f55760c47fa94605258adf
Zhang Hao <2162990048@qq.com>
2018-07-01 11:17:14 +0800

    Create songle.c
    继电器

---
+++ arduino/songle.c
@@ -0,0 +1,13 @@
+int pinRelay = 3; //管脚D3连接到继电器模块的信号脚
+ 
+void setup() {
+  pinMode(pinRelay, OUTPUT); //设置pinRelay脚为输出状态
+}
+ 
+void loop() { 
+   digitalWrite(pinRelay, HIGH);//输出HIGH电平,继电器模块闭合
+   delay(5000); //等待5000毫秒
+ 
+   digitalWrite(pinRelay, LOW);//输出LOW电平,继电器模块断开
+   delay(8000); //等待8000毫秒
+}
